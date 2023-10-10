7787b1d0beedd2caa6829eebe2d77cbce54dd197
TkDevk <duvanuriel@hotmail.com>
2023-10-10 13:22:57 -0500

    Desafio completado

---
+++ script.js
@@ -0,0 +1,24 @@
+function fahrenheit() {
+    let temperaturaResultado=0;
+    const temperaturaInput = document.getElementById("temperatura");
+    const temperatura = parseFloat(temperaturaInput.value);
+    temperaturaResultado = (temperatura* (9/5)) + 32; 
+    console.log(temperaturaResultado);
+
+    const mostrar = document.getElementById('mostrarF');
+    mostrar.textContent=`El resultado de la conversion a Fahrenheit es :${temperaturaResultado}`;
+
+}
+
+function kelvin() {
+    let temperaturaResultado=0;
+    const temperaturaInput = document.getElementById("temperatura");
+    const temperatura = parseFloat(temperaturaInput.value);
+    temperaturaResultado = temperatura + 273.15 ; 
+    console.log(temperaturaResultado);
+
+    const mostrar = document.getElementById('mostrarK');
+    mostrar.textContent=`El resultado de la conversion a Kelvin es :${temperaturaResultado}`;
+
+}
+
